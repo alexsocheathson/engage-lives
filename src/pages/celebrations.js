3e012a10df640f34932b4aaf7b106b758c423a91
Alex Son <alexson28@gmail.com>
2018-07-27 16:33:24 -0500

    as-contentful

--- src/pages/celebrations.js
+++ src/pages/celebrations.js
@@ -2,12 +2,54 @@ import React from 'react'
 
 class Celebration extends React.Component {
   render(){
+    const pageContent = this.props.data.allContentfulCelebrationPage.edges
+    const celebrationContent = this.props.data.allContentfulCelebrations.edges
+
+    const createList = celebrationContent.map((celebration) =>
+      <div key={celebration.node.eventName} >
+        <h2>{celebration.node.eventName}</h2>
+        <p>{celebration.node.date.substring(0,10)}</p>
+        <h5>{celebration.node.teaser}</h5>
+        <p>{celebration.node.eventDetails.eventDetails}</p>
+        <p><a href={celebration.node.url}>{celebration.node.url}</a></p>
+        <p>{celebration.node.publishingChurch}</p>
+      </div>
+    )
+
     return(
       <div>
-        Celebration page
+        <h1>Celebration</h1>
+        <h4>{pageContent["0"].node.heading}</h4>
+        {createList}
       </div>
     );
   }
 }
 
 export default Celebration
+
+export const query = graphql`
+  query CelebrationQuery {
+    allContentfulCelebrationPage {
+      edges {
+        node {
+          heading
+        }
+      }
+    }
+    allContentfulCelebrations {
+      edges {
+       node {
+          eventName
+          teaser
+          date
+          url
+          eventDetails {
+           eventDetails
+          }
+          publishingChurch
+        }
+      }
+    }
+  }
+`
